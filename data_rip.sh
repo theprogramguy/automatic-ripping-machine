6f940afc57490ffc5c527c1fa969bbcb3e56ce24
James <jimmypopali96@gmail.com>
2017-02-05 20:35:16 -0800

    Fixed Eject

--- data_rip.sh
+++ data_rip.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-# Rip Data using DD
-
 # shellcheck disable=SC1091
 # shellcheck source=config
+# Rip Data using DD
 source "$ARM_CONFIG"
+exec >> "$LOG"
+exec 2>&1
 
-{
-
-        TIMESTAMP=$(date '+%Y%m%d_%H%M%S');
-        DEST="/mnt/media/ARM/Media/Data/${TIMESTAMP}_${ID_FS_LABEL}"
-        mkdir -p "$DEST"
-	FILENAME=${ID_FS_LABEL}_disc.iso
+TIMESTAMP=$(date '+%Y%m%d_%H%M%S');
+DEST="/mnt/media/ARM/Media/Data/${TIMESTAMP}_${ID_FS_LABEL}"
+mkdir -p "$DEST"
+FILENAME=${ID_FS_LABEL}_disc.iso
 
 
-	#dd if=/dev/sr0 of=$DEST/$FILENAME 
-	cat "$DEVNAME" > "$DEST/$FILENAME"
+#dd if=/dev/sr0 of=$DEST/$FILENAME 
+cat "$DEVNAME" > "$DEST/$FILENAME"
+eject "$DEVNAME"
 
-	eject
-	
-	if [ "$SET_MEDIA_PERMISSIONS" = true ]; then
-			
-	chmod -R "$CHMOD_VALUE" "$DEST"
-		
-	fi
+if [ "$SET_MEDIA_PERMISSIONS" = true ]; then
+	chmod -R "$CHMOD_VALUE" "$DEST"	
+fi
 
-} >> "$LOG"
